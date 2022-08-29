59e7616f1cfe3bf663fde1cc483523974cc2e671
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-08-29 20:43:25 +0000

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Temporal } from "@js-temporal/polyfill";
 
 function App() {
-  const [time, setTime] = useState(new Date());
+  const [instant, setInstant] = useState(Temporal.Now.instant());
+  const [date, setDate] = useState(0);
+  const [time, setTime] = useState(0);
+  const offsetSeconds = useRef(
+    Temporal.Now.timeZone().getOffsetNanosecondsFor(Temporal.Now.instant()) /
+      1e9
+  );
+  const [dateTime, setDateTime] = useState(date + time - offsetSeconds.current);
 
-  const updateTime = () => setInterval(() => setTime(new Date()), 1000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setInstant(Temporal.Now.instant());
+      setDateTime(date + time - offsetSeconds.current);
+    });
+    return () => clearInterval(interval);
+  });
 
-  updateTime();
+  const handleDatePicker = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(evt.target.valueAsNumber / 1000);
+  };
+
+  const handleTimePicker = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setTime(evt.target.valueAsNumber / 1000);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <input type={"date"} onChange={handleDatePicker}></input>
+          <input type={"time"} onChange={handleTimePicker}></input>
         </p>
-        <pre>{(time.getTime() / 1000).toFixed(0)}</pre>
+        <pre>
+          date: {date}
+          <br />
+          time: {time}
+          <br />
+          sel: {dateTime}
+          <br />
+          cur: {instant.epochSeconds}
+        </pre>
       </header>
     </div>
   );
