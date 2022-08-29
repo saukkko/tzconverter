1c6e7caf0cc16f30f12c8196e05c9aab64466d65
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-08-29 18:38:32 +0000

    added test content

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(new Date());
+
+  const updateTime = () => setInterval(() => setTime(new Date()), 1000);
+
+  updateTime();
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <pre>{(time.getTime() / 1000).toFixed(0)}</pre>
       </header>
     </div>
   );
